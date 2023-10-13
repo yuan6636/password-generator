b92a8782ef38376e6224201a07b91a5d061e0c77
yuan6636 <aleniverson777@gmail.com>
2023-10-13 15:51:57 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/generate', (req, res) => {
+  
+})
+
 app.listen(port, () => {
   console.log(`express server on http://localhost:${port}`)
 })
